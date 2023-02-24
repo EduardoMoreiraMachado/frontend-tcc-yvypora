a040b23dc5c10c130d827a387c1b11a130c939af
Eddie The Bill <dudumm1234@gmail.com>
2023-02-23 21:36:59 -0300

    implementando react hooks

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,12 +1,113 @@
 import './style.css';
-import { Component } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { loadPosts } from '../../utils/load-posts';
 import { Posts } from '../../components/Posts';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 
+/*************************************************************************************************************
+* COMPONENTE *
+* trecho de código da aplicação que é possível encapsular em uma função ou classe, sendo possível reaproveitar
+**************************************************************************************************************
+* PROPS *
+* são atributos que são passados nos componentes
+**************************************************************************************************************
+* STATE *
+* uma forma de armazenar um valor dentro de um componente, sendo possível renderizar ele ou passar informações
+*/
+
+// componente de função
+export const Home = () => {
+
+  // o use state indica o valor inicial e retorna um vetor
+  // ele precisa do valor inicial e de uma função que atualiza o valor do estado
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postPerPage] = useState(10);
+  const [searchValue, setSearchValue] = useState('');
+
+  const noMorePosts = page + postPerPage >= allPosts.length;
+
+  const filteredPosts = !!searchValue ? 
+      // se houver valor em 'searchValue' irá filtrar todos os posts que contém o valor de 'searchValue' como título
+      allPosts.filter(post => {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase());
+      }) 
+      // se não houver valor, irá retornar somente os posts
+    : posts;
+
+  const handleLoadPosts = useCallback(async (page, postPerPage) => {
+
+    const postsAndPhotos = await loadPosts();
+
+    setPosts(postsAndPhotos.slice(page, postPerPage));
+    setAllPosts(postsAndPhotos)
+
+  }, [])
+
+  useEffect(() => {
+
+    handleLoadPosts(0, postPerPage)
+
+  // as variáveis indicam quando que o useEffect dever ser chamado, quando uma das declaradas mudarem
+  }, [handleLoadPosts, postPerPage]);
+
+  const loadMorePosts = () => {
+  
+    const nextPage = page + postPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postPerPage)
+    posts.push(...nextPosts);
+
+    setPosts(posts);
+    setPage(nextPage);
+
+  } 
+
+  const handleChange = (event) => {
+
+    const {value} = event.target;
+
+    setSearchValue(value)
+
+  }  
+
+  return (
+
+    <section className='container'>
+      <div className="search-container">
+        {!!searchValue && (
+          <h1>Search value: {searchValue}</h1>
+        )}
+        <Input
+          searchValue={searchValue}
+          handleChange={handleChange} 
+        />
+      </div>
+      {filteredPosts.length > 0 && (
+        <Posts posts={filteredPosts} />
+      )}
+      {filteredPosts.length === 0 && (
+        <p>Não existem posts</p>
+      )}
+      <div className="button-container">
+        {!searchValue && (
+          <Button 
+            text="Load more posts"
+            onClick={loadMorePosts} 
+            disabled={noMorePosts}
+          />
+        )}
+      </div>
+    </section>
+
+  );  
+
+}
+
+/* 
 // componente de classe
-class Home extends Component {
+export class Home2 extends Component {
   // responsável por gerenciar o estado do componente
   // toda vez que o estado muda, o método render é chamado de novo 
   state = {
@@ -92,5 +193,6 @@ class Home extends Component {
     );  
   }
 }
+*/
 
 export default Home;
