ccbe422d6b5200d960f6701585fd8915c1a5aff1
HeitorPontieri <heitorpontieri1@gmail.com>
2023-03-15 10:58:14 -0300

    near fair page ok

--- src/templates/FairNear/index.jsx
+++ src/templates/FairNear/index.jsx
@@ -7,36 +7,86 @@ import { Footer } from '../../components/Footer'
 
 export const FairNear = () =>{
     return(
-        <div>
+        <div className='all-content-container'>
             <EmptyHeader/>
             <Title text={'Feiras Próximas'}/>
-            div.
-            <NavBar/>
-            <FairNearCard 
-                title={'Feira Livre da Rua Oscar Freire'}
-                img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
-                note={3.5}
-                dayWorkText ={'Domingo'}
-                hourWorkText = {'08:00 - 16:00'}
-                fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
-            />
-            {/* <FairNearCard 
-                title={'Feira Noturna de Barueri'}
-                img={'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/8f/d6/a8/a-movimentacao-da-feira.jpg?w=1200&h=1200&s=1'} 
-                note={4.5}
-                dayWorkText ={'Terça'}
-                hourWorkText = {'18:00 - 23:00'}
-                fairmanText = {'Aprox 56 feirantes cadastrados no yvy'}
-            />
-             <FairNearCard 
-                title={'Feira Livre da Rua Oscar Freire'}
-                img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
-                note={3.5}
-                dayWorkText ={'Domingo'}
-                hourWorkText = {'08:00 - 16:00'}
-                fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
-            /> */}
-            
+           <div className="nav-card-container">
+                <NavBar/>
+                <div className="cards">
+                    <FairNearCard 
+                        title={'Feira Livre da Rua Oscar Freire'}
+                        img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
+                        note={3.5}
+                        dayWorkText ={'Domingo'}
+                        hourWorkText = {'08:00 - 16:00'}
+                        fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
+                    />
+                    <FairNearCard 
+                        title={'Feira Livre da Rua Oscar Freire'}
+                        img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
+                        note={3.5}
+                        dayWorkText ={'Domingo'}
+                        hourWorkText = {'08:00 - 16:00'}
+                        fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
+                    />
+                    <FairNearCard 
+                        title={'Feira Livre da Rua Oscar Freire'}
+                        img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
+                        note={3.5}
+                        dayWorkText ={'Domingo'}
+                        hourWorkText = {'08:00 - 16:00'}
+                        fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
+                    />
+                    <FairNearCard 
+                        title={'Feira Livre da Rua Oscar Freire'}
+                        img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
+                        note={3.5}
+                        dayWorkText ={'Domingo'}
+                        hourWorkText = {'08:00 - 16:00'}
+                        fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
+                    />
+                    <FairNearCard 
+                        title={'Feira Livre da Rua Oscar Freire'}
+                        img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
+                        note={3.5}
+                        dayWorkText ={'Domingo'}
+                        hourWorkText = {'08:00 - 16:00'}
+                        fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
+                    />
+                    <FairNearCard 
+                        title={'Feira Livre da Rua Oscar Freire'}
+                        img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
+                        note={3.5}
+                        dayWorkText ={'Domingo'}
+                        hourWorkText = {'08:00 - 16:00'}
+                        fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
+                    />
+                    <FairNearCard 
+                        title={'Feira Livre da Rua Oscar Freire'}
+                        img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
+                        note={3.5}
+                        dayWorkText ={'Domingo'}
+                        hourWorkText = {'08:00 - 16:00'}
+                        fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
+                    />
+                    <FairNearCard 
+                        title={'Feira Livre da Rua Oscar Freire'}
+                        img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
+                        note={3.5}
+                        dayWorkText ={'Domingo'}
+                        hourWorkText = {'08:00 - 16:00'}
+                        fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
+                    />
+                    <FairNearCard 
+                        title={'Feira Livre da Rua Oscar Freire'}
+                        img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
+                        note={3.5}
+                        dayWorkText ={'Domingo'}
+                        hourWorkText = {'08:00 - 16:00'}
+                        fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
+                    />
+                </div>
+            </div>
             <Footer/>
 
                 
