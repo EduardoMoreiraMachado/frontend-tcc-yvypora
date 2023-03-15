c33b4ed0d24ac6ea245d8ba50200cca9f3a0db75
HeitorPontieri <heitorpontieri1@gmail.com>
2023-03-15 09:39:31 -0300

    near fair card

--- src/components/FairNearCard/index.jsx
+++ src/components/FairNearCard/index.jsx
@@ -1,34 +1,45 @@
 import './style.css'
 import IconStar from '../../imgs/icon_star.svg'
 import IconWork from '../../imgs/clock.svg'
-import IconCalendar from '../../imgs/canlendartick.svg'
+import IconCalendar from '../../imgs/calendartick.svg'
 import IconFairmen from '../../imgs/icon_feirante.svg'
+import { NavigationType } from 'react-router-dom'
 
 
-export const FairNearCard = ({title},{note},{dayWorkText}, {hourWorkText},{fairmanText}) =>{
+export const FairNearCard = ({title,img,note,dayWorkText, hourWorkText,fairmanText}) =>{
+    // let t = title
+    // let n = note
+    // let d = dayWorkText
+    // let h = hourWorkText
+    // let f = fairmanText
+
+    // console.log(t,n,d,h,f);
     return (
+        <div className="main-card">
         <div className="card-container">
+                <img src={img} alt="" className="img-card"/>
+            <div className="content-container">
             <div className="header-card">
                  <h2>{title}</h2>
-                 <img src={IconStar} alt="" />
-                 <p>{note}</p>
+                <div className="note-container">
+                    <img src={IconStar} alt="" />
+                    <p>{note}</p>
+                 </div>
             </div>
-            <div className="day-work-container">
-                <p>{dayWorkText}</p>
+            <div className="containers">
                 <img src={IconWork} alt="" />
-            </div>
-            <div className="hour-work-container">
                 <p>{dayWorkText}</p>
+            </div>
+            <div className="containers">
                 <img src={IconCalendar} alt="" />
+                <p>{hourWorkText}</p>
             </div>
-            <div className="fairmen-container">
-                <p>{fairmanText}</p>
+            <div className="containers">
                 <img src={IconFairmen} alt="" />
+                <p>{fairmanText}</p>
             </div>
-
-           
-
-
+            </div>
+        </div>
         </div>
     )
 }
--- src/templates/FairNear/index.jsx
+++ src/templates/FairNear/index.jsx
@@ -3,16 +3,46 @@ import {EmptyHeader} from '../../components/EmptyHeader'
 import {NavBar} from '../../components/NavBar'
 import Title from '../../components/Title'
 import FairNearCard from '../../components/FairNearCard'
+import { Footer } from '../../components/Footer'
 
 export const FairNear = () =>{
     return(
         <div>
             <EmptyHeader/>
             <Title text={'Feiras Próximas'}/>
+            div.
             <NavBar/>
-            <FairNearCard title={'Feira Livre da Rua Oscar Freire'} />
+            <FairNearCard 
+                title={'Feira Livre da Rua Oscar Freire'}
+                img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
+                note={3.5}
+                dayWorkText ={'Domingo'}
+                hourWorkText = {'08:00 - 16:00'}
+                fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
+            />
+            {/* <FairNearCard 
+                title={'Feira Noturna de Barueri'}
+                img={'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/8f/d6/a8/a-movimentacao-da-feira.jpg?w=1200&h=1200&s=1'} 
+                note={4.5}
+                dayWorkText ={'Terça'}
+                hourWorkText = {'18:00 - 23:00'}
+                fairmanText = {'Aprox 56 feirantes cadastrados no yvy'}
+            />
+             <FairNearCard 
+                title={'Feira Livre da Rua Oscar Freire'}
+                img={'https://conteudo.solutudo.com.br/wp-content/uploads/2019/10/15220191645824798_5zxyp.jpg'} 
+                note={3.5}
+                dayWorkText ={'Domingo'}
+                hourWorkText = {'08:00 - 16:00'}
+                fairmanText = {'Aprox 4 feirantes cadastrados no yvy'}
+            /> */}
+            
+            <Footer/>
+
+                
         </div>
     )
 }
+
 
 export default FairNear
