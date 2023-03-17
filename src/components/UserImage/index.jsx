7964bc5b5610b19a76ce39c3b81dfd0b4a5a8400
HeitorPontieri <heitorpontieri1@gmail.com>
2023-03-17 11:08:38 -0300

    'main'

--- src/components/UserImage/index.jsx
+++ src/components/UserImage/index.jsx
@@ -7,3 +7,4 @@ export const UserImage = ({imgUrl}) => {
         </div>
     )
 }
+export default UserImage
---
+++ src/templates/BuyHistory/index.jsx
@@ -0,0 +1,33 @@
+import "./style.css"
+import { EmptyHeader } from "../../components/EmptyHeader"
+import Title from "../../components/Title"
+import UserImage from "../../components/UserImage"
+import InfoBuy from "../../components/InfoBuy"
+import iconYvY from "../../imgs/yvypora_icon.svg"
+
+export const BuyHistory = () =>{
+    return (
+        <div className="buy-history-container">
+            <div className="header-buy-container">
+                <div className="iconyvy-container">
+                    <img src={iconYvY} alt=""  className="icon-yvy"/>
+                </div>
+                <Title text={'Histórico de Compras'}/>
+                <UserImage                     
+                    imgUrl={'https://www.citypng.com/public/uploads/preview/download-profile-user-round-orange-icon-symbol-png-11639594360ksf6tlhukf.png'}
+                />
+            </div>
+            <div className="main-buy-container">
+            <InfoBuy 
+                imgURL={"https://blog.sansuy.com.br/wp-content/uploads/2020/04/original-1f9941da9ca09fa415d6a0139a3dc33a-710x474.jpg"}
+                nameCostumer={"Barraca do Seu Zé"}
+                nameFair={"Feira de Barueri"}
+                date={'31/02/23'}/>
+            </div>
+
+        </div>
+    )
+
+}
+
+export default BuyHistory
