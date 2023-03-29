95b7218e1572f727e2fbab736e5c5abdb4dd6f63
Eduardo Moreira Machado <dudumm1234@gmail.com>
2023-03-29 11:26:19 -0300

    container com parte da atividade do feirante para a página inicial de feirante

--- src/templates/FeiranteStartPage/index.jsx
+++ src/templates/FeiranteStartPage/index.jsx
@@ -1,6 +1,5 @@
 import './style.css'
 
-import { UserImage } from '../../components/UserImage'
 import { NavBar } from '../../components/NavBar'
 import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
@@ -8,19 +7,20 @@ import { Header } from '../../components/Header'
 import GreenAddIcon from '../../imgs/green_add_icon.svg'
 import AddProductsIcon from '../../imgs/add_products_icon.png'
 import AddFairsIcon from '../../imgs/add_fairs_icon.png'
-import HeaderGreenWave from '../../imgs/header_green_wave.svg'
+import EyeVisible from '../../imgs/eye_visible.png'
+import EyeNotVisible from '../../imgs/eye_not_visible.png'
+
+import { useState } from 'react'
 
 export const FeiranteStartPage = () => {
+    const [ isVisible, setIsVisible ] = useState(true);
+
+    function toggleVisibility() {
+        setIsVisible(!isVisible)
+    }
+
     return(
         <div className='feirante-start-page-container'>
-            {/* <header>
-                <img src={HeaderGreenWave}/>
-                <div className='user-image'>
-                    <UserImage 
-                        imgUrl="https://wl-incrivel.cf.tsp.li/resize/728x/jpg/91b/430/964a9c5ac9933cc012d0bd80be.jpg"
-                    />
-                </div>
-            </header> */}
             <Header 
                 imgUrl="https://wl-incrivel.cf.tsp.li/resize/728x/jpg/91b/430/964a9c5ac9933cc012d0bd80be.jpg"
             />
@@ -43,6 +43,23 @@ export const FeiranteStartPage = () => {
                                 </div>
                                 <img className='fairs-icon' src={AddFairsIcon} alt=""/>
                             </div>
+                        </div>
+                    </div>
+                    <div className='feirante-activity'>
+                        <div className='half-historic'>
+                            <h1>Sua atividade hoje</h1>
+                            <h2>Ganhos</h2>
+                            <div className='money-visibility'>
+                                <h1 style={isVisible ? {color: '#245501'} : {color: 'transparent'}}>R$ 0,00</h1>
+                                <button onClick={toggleVisibility} style={isVisible ? {backgroundImage: `url(${EyeNotVisible})`} : {backgroundImage: `url(${EyeVisible})`}}/>
+                            </div>
+                            <div className='sales'>
+                                <h3>Venda às 12:59</h3>
+                                <span>R$ 5,00</span>
+                            </div>
+                        </div>
+                        <div className='all-historic-button'>
+                            <h1>Ver histórico completo</h1>
                         </div>
                     </div>
                 </div>
