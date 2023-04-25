15eea4219b24733ce94a2964ba6dab554868e8a6
Eduardo Moreira Machado <dudumm1234@gmail.com>
2023-04-25 11:24:55 -0300

    página de confirmação de pagamento

--- src/templates/PaymentPage/index.jsx
+++ src/templates/PaymentPage/index.jsx
@@ -1,30 +1,136 @@
 import styles from './styles.module.css'
-import { PrevButton } from "../../components/PrevButton"
-import { GreenButton } from "../../components/GreenButton"
-import { Title } from "../../components/Title"
-import { Footer } from "../../components/Footer"
-import { Option } from "../../components/Option"
 
-export const PaymentPage = () =>{
+import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
+import { BoughtItem } from '../../components/BoughtItem'
+import { PrevButton } from '../../components/PrevButton'
+import { NextButton } from '../../components/NextButton'
+import { GreenButton } from '../../components/GreenButton'
+import { WhiteButton } from '../../components/WhiteButton'
+
+import { useRef } from 'react'
+
+export const PaymentPage = () => {
+    const carousel = useRef(null);
+
+    const handleLeftClick = (e) => {
+        e.preventDefault();
+    
+        carousel.current.scrollLeft -= carousel.current.offsetWidth;
+    };
+
+    const handleRightClick = (e) => {
+        e.preventDefault();
+        carousel.current.scrollLeft += carousel.current.offsetWidth;
+    };
+
     return(
-        <div className={styles["main-payment-container"]}>
-            <div className={styles["options-payment-container"]}>
-                <div className={styles["credit-card-container"]}>
-                    <p>Cartão de Crédito</p>
-                    <Option text={"VISA"} 
-                            imgUrl={"https://cdn-icons-png.flaticon.com/512/1/1863.png"}/>
-                    <Option text={"Mastercard"} 
-                            imgUrl={"https://logospng.org/wp-content/uploads/mastercard.jpg"}/>
+        <div className={styles['payment-page-container']}>
+            <div className={styles['payment-content']}>
+
+                <div className={styles['itens-info']}>
+
+                    <div className={styles['address-card']}>
+                        <div className={styles['card-header']}>
+                            <h1>Endereço</h1>
+                            <a href='#'>Mudar</a>
+                        </div>
+                        <div className={styles['card-main']}>
+                            <h1>Casa</h1>
+                            <p>Rua Oscar Freire nº126, São Paulo, São Paulo. Brasil</p>
+                        </div>
+                    </div>
+
+                    <div className={styles['historic']}>
+                        <div className={styles['purchase']}>
+                            <div className={styles['main-purchase-info']}>
+                                <div className={styles['purchase-image']} style={{backgroundImage: `url('https://pocosdecaldas.mg.gov.br/wp-content/uploads/2020/05/WhatsApp-Image-2020-04-09-at-09.41.39-2-1024x768.jpeg')`}}></div>
+                                <div className={styles['purchase-info']}>
+                                    <h1>Barraca do Seu Zé</h1>
+                                    <h2>Feira de São Domingos</h2>
+                                    <span>Data: 41/13/2027</span>
+                                </div>
+                            </div>
+                            <div className={styles['purchase-products']} ref={carousel}>
+                                <BoughtItem 
+                                    name='Abóbora'
+                                    imgUrl='https://img.freepik.com/free-photo/close-up-shot-fresh-pumpkins-different-shapes-sizes-perfect_181624-31370.jpg?w=2000&t=st=1681383871~exp=1681384471~hmac=3925907f5157d0f6192b61c3c1ca599433a696bddb2599bf8c69ce954fd2a457'
+                                    unit='800g'
+                                    price='R$ 24,00'
+                                    qnt={4}
+                                />
+                                <BoughtItem 
+                                    name='Abóbora'
+                                    imgUrl='https://img.freepik.com/free-photo/close-up-shot-fresh-pumpkins-different-shapes-sizes-perfect_181624-31370.jpg?w=2000&t=st=1681383871~exp=1681384471~hmac=3925907f5157d0f6192b61c3c1ca599433a696bddb2599bf8c69ce954fd2a457'
+                                    unit='800g'
+                                    price='R$ 24,00'
+                                    qnt={4}
+                                />
+                                <BoughtItem 
+                                    name='Abóbora'
+                                    imgUrl='https://img.freepik.com/free-photo/close-up-shot-fresh-pumpkins-different-shapes-sizes-perfect_181624-31370.jpg?w=2000&t=st=1681383871~exp=1681384471~hmac=3925907f5157d0f6192b61c3c1ca599433a696bddb2599bf8c69ce954fd2a457'
+                                    unit='800g'
+                                    price='R$ 24,00'
+                                    qnt={4}
+                                />
+                                <BoughtItem 
+                                    name='Abóbora'
+                                    imgUrl='https://img.freepik.com/free-photo/close-up-shot-fresh-pumpkins-different-shapes-sizes-perfect_181624-31370.jpg?w=2000&t=st=1681383871~exp=1681384471~hmac=3925907f5157d0f6192b61c3c1ca599433a696bddb2599bf8c69ce954fd2a457'
+                                    unit='800g'
+                                    price='R$ 24,00'
+                                    qnt={4}
+                                />
+                                <BoughtItem 
+                                    name='Abóbora'
+                                    imgUrl='https://img.freepik.com/free-photo/close-up-shot-fresh-pumpkins-different-shapes-sizes-perfect_181624-31370.jpg?w=2000&t=st=1681383871~exp=1681384471~hmac=3925907f5157d0f6192b61c3c1ca599433a696bddb2599bf8c69ce954fd2a457'
+                                    unit='800g'
+                                    price='R$ 24,00'
+                                    qnt={4}
+                                />
+                                <BoughtItem 
+                                    name='Abóbora'
+                                    imgUrl='https://img.freepik.com/free-photo/close-up-shot-fresh-pumpkins-different-shapes-sizes-perfect_181624-31370.jpg?w=2000&t=st=1681383871~exp=1681384471~hmac=3925907f5157d0f6192b61c3c1ca599433a696bddb2599bf8c69ce954fd2a457'
+                                    unit='800g'
+                                    price='R$ 24,00'
+                                    qnt={4}
+                                />
+                                <BoughtItem 
+                                    name='Abóbora'
+                                    imgUrl='https://img.freepik.com/free-photo/close-up-shot-fresh-pumpkins-different-shapes-sizes-perfect_181624-31370.jpg?w=2000&t=st=1681383871~exp=1681384471~hmac=3925907f5157d0f6192b61c3c1ca599433a696bddb2599bf8c69ce954fd2a457'
+                                    unit='800g'
+                                    price='R$ 24,00'
+                                    qnt={4}
+                                />
+                            </div>
+                            <div className={styles['nav-buttons']}>
+                                <PrevButton onClick={handleLeftClick}/>
+                                <NextButton onClick={handleRightClick}/>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-                <div className={styles["others-container"]}>
-                    <p>Outros</p>
-                    <Option  text={"PayPal"} imgUrl={"https://cdn-icons-png.flaticon.com/512/174/174861.png"}/>
-                    <Option text={"ApplePay"} imgUrl={"https://developer.apple.com/news/images/og/apple-pay-og-twitter.jpg"}/>
+
+                <div className={styles['payment-info']}>
+                    <div className={styles['payment-texts']}>
+                        <span>Subtotal: R$ 48,00</span>
+                        <span>Taxa de entrega: R$ 8,32</span>
+                        <p>Total: <h1>R$ 56,32</h1></p>
+                    </div>  
+                    <div className={styles['confirm-buttons']}>
+                        <GreenButton
+                            text='Confirmar' 
+                        />
+                        <WhiteButton
+                            text='Cancelar' 
+                        />
+                    </div>
                 </div>
-                <GreenButton text={"Adicionar"} type={"submit"}/>
+
             </div>
-            <Footer/>
-        </div>  
+            <Footer />
+        </div>
     )
 }
+
 export default PaymentPage
---
+++ src/templates/PaymentPageDISABLED/index.jsx
@@ -0,0 +1,29 @@
+import styles from './styles.module.css'
+import { PrevButton } from "../../components/PrevButton"
+import { GreenButton } from "../../components/GreenButton"
+import { Title } from "../../components/Title"
+import { Footer } from "../../components/Footer"
+import { Option } from "../../components/Option"
+
+export const PaymentPageDISABLED = () =>{
+    return(
+        <div className={styles["main-payment-container"]}>
+            <div className={styles["options-payment-container"]}>
+                <div className={styles["credit-card-container"]}>
+                    <p>Cartão de Crédito</p>
+                    <Option text={"VISA"} 
+                            imgUrl={"https://cdn-icons-png.flaticon.com/512/1/1863.png"}/>
+                    <Option text={"Mastercard"} 
+                            imgUrl={"https://logospng.org/wp-content/uploads/mastercard.jpg"}/>
+                </div>
+                <div className={styles["others-container"]}>
+                    <p>Outros</p>
+                    <Option  text={"PayPal"} imgUrl={"https://cdn-icons-png.flaticon.com/512/174/174861.png"}/>
+                    <Option text={"ApplePay"} imgUrl={"https://developer.apple.com/news/images/og/apple-pay-og-twitter.jpg"}/>
+                </div>
+                <GreenButton text={"Adicionar"} type={"submit"}/>
+            </div>
+            <Footer/>
+        </div>  
+    )
+}
