c68b3cbd1c2f94e43499e86d58552e6eddec91d1
GUILHERME JOVINIANO DE SOUSA <22102359@CFP127ALUNO.LOCAL>
2023-06-06 11:17:32 -0300

    add integration if fair already exist

--- src/templates/SellerProductsPage/index.jsx
+++ src/templates/SellerProductsPage/index.jsx
@@ -48,7 +48,7 @@ export const SellerProductsPage = () => {
         </div>
       </div> */}
       <div className={styles['seller-products-content']}>
-        {!listOfProducts ?
+        {listOfProducts.length === 0 ?
           <div className={styles['history-not-found']}>
             <DataNotFound 
               text='Você ainda não tem produtos cadastrados!'
